facac99b0341410a0b730cdb89043cbde8297350
Lindsay Walker <lawalker98@gmail.com>
2023-10-31 16:01:45 -0400

    boilerplate Home with imports

---
+++ meals-hackathon/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import Main from './Main'
+import Nav from './Nav'
+
+
+export default function Home (){
+    return (
+        
+        <div className="home">
+            <Nav/>
+            <Main/>
+        </div>
+)
+}
