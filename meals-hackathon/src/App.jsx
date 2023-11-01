fd6a71c445632e899239fff124be70dca486b0fa
Lindsay Walker <lawalker98@gmail.com>
2023-10-31 20:21:27 -0400

    basic outline for site

--- meals-hackathon/src/App.jsx
+++ meals-hackathon/src/App.jsx
@@ -1,13 +1,18 @@
-import { useState } from 'react'
+// import { useState, useEffect } from 'react'
 import Home from './components/Home'
 import './App.css'
+import Header from './components/Header'
+// import { BASE_URL } from './globals'
+// import axios from 'axios'
+// import { useNavigate } from 'react-router-dom'
 
 
 function App() {
 
 
   return (
-    <div className='App'>
+    <div className='app'>
+      <Header/>
       <Home/>
     </div>
 
---
+++ meals-hackathon/src/components/Nav.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom'
+
+export default function Nav (){
+    return (
+        
+        <div className="nav">
+            <div id="nav-itm"><Link to="/"> Home </Link></div>
+            <div id="nav-itm"><Link to="/Categories"> Categories </Link></div>
+            <div id="nav-itm"><Link to="/Contact"> Contact </Link></div>
+        </div>
+)
+}
