63902432f85783671d8fa9e7570c478f5713096b
Lindsay Walker <lawalker98@gmail.com>
2023-11-01 11:29:13 -0400

    updataed homepage with axios call

--- meals-hackathon/src/components/Header.jsx
+++ meals-hackathon/src/components/Header.jsx
@@ -7,12 +7,7 @@ export default function Header (){
         <div className="header">
                 <Nav/>     
             <h1 className='title'>Meals Hackathon</h1>
-            <div id="search">
-                    <form>
-                        <input placeholder="search" id="search-bar"></input>
-                        <button id="submit-search">Enter</button>
-                    </form>
-             </div>
+
 
 
         </div>
