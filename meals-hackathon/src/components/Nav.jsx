a28c60059f2456c90a4fa69f140860a3f6c0d8a6
Lindsay Walker <lawalker98@gmail.com>
2023-11-01 20:32:24 -0400

    end-of-day

--- meals-hackathon/src/components/Nav.jsx
+++ meals-hackathon/src/components/Nav.jsx
@@ -1,13 +1,23 @@
 import { Link } from 'react-router-dom'
 
+
 export default function Nav (){
+
+
+
+
     return (
-        
-        <div className="nav">
-            <div id="nav-itm"><Link to="/"> Home </Link></div>
-            <div id="nav-itm"><Link to="/Categories"> Categories </Link></div>
-            <div id="nav-itm"><Link to="/Contact"> Contact </Link></div>
-        </div>
+        <div>
+            <div className="nav">
+                <div className="navItm"><Link to="/"> Home </Link></div>
+                <div className="navItm"><Link to="/Categories"> Categories </Link></div>
+                <div className="navItm"><Link to="/Contact"> Contact </Link></div>
+            </div>
+            {/* <button id="surprise" onClick={() => {showModal= true}}>Surprise</button> */}
+
+
+            
+         </div>
 )
 }
 
